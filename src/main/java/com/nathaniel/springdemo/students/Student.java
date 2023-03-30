dcdba0d5d30caeba48725dff592c2bb03f68ca28
Nathaniel <kolawolenathaniel26@gmail.com>
2023-03-30 16:43:19 +0100

    Built the projects first API

---
+++ src/main/java/com/nathaniel/springdemo/students/Student.java
@@ -0,0 +1,15 @@
+package com.nathaniel.springdemo.students;
+
+import java.time.LocalDate;
+
+public class Student {
+
+    private String firstname;
+
+    private String lastname;
+
+    private String email;
+    private int age;
+
+    private LocalDate dateofbirth;
+}
